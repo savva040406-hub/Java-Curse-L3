6b66a7058235d9e55a3f249817842b5d89164d90
savva040406-hub <savva040406@gmail.com>
2026-05-30 20:54:31 +0300

    Add files via upload

---
+++ ConsoleGreetingPractice.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class ConsoleGreetingPractice {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите имя: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Привет, " + name);
+    }
+}
